b6c8b9e32c38100e303a3650634c14408e25650f
Irsyad Zaki Azhar <emailkamu@gmail.com>
2026-09-07 14:37:14 +0700

    first commit

---
+++ JS/js script.js
@@ -0,0 +1,6 @@
+const promoButton = document.querySelector("#promoButton");
+
+promoButton.addEventListener("click", () => {
+    promoButton.textContent = "promo beli 1 gratis 1!";
+    console.log("promo kopi nusa berhasil ditampilkan.");
+})
